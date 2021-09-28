2db73dfa92ad3b979326217c8dc082b30c41393a
Flamey <morellialex08@gmail.com>
2021-09-28 19:45:15 +0200

    2.0.0a

---
+++ EggsBot/cogs/developer.py
@@ -0,0 +1,75 @@
+import discord
+import discord_slash as interactions
+import json
+import asyncpg
+import aiohttp
+from discord_slash import cog_ext
+from discord.ext import commands
+
+class Developer(commands.Cog):
+    "Developer-only stuff."
+
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+        with open("./token.json") as tokenfile:
+            self.token = json.load(tokenfile)
+        with open("./postgres.json") as postgresfile:
+            self.postgres = json.load(postgresfile)
+
+    async def pgexecute(self, sql: str, stuff: str=None):
+        db: asyncpg.Connection = await asyncpg.connect(self.postgres["eggs"])
+        if stuff is None:
+            await db.execute(f'''{sql}''')
+        else:
+            await db.execute(f'''{sql}''', stuff)
+
+    async def pgselect(self, query: str):
+        db: asyncpg.Connection = await asyncpg.connect(self.postgres["eggs"])
+        return await db.fetchrow(f'''{query}''')
+
+    @cog_ext.cog_subcommand(base="db", subcommand_group="table", name="create", description="Developer - Creates a table in the SEggsAPI DB.", base_default_permission=False, base_permissions={
+        832594030264975420: [
+            interactions.utils.manage_commands.create_permission(450678229192278036, 2, True)
+        ],
+        838718002412912661: [
+            interactions.utils.manage_commands.create_permission(450678229192278036, 2, True)
+        ]
+    })
+    async def dbtablecreate(self, ctx: interactions.SlashContext, name: str, stuff: str):
+        await self.pgexecute(f"CREATE TABLE {name} ({stuff})")
+        await ctx.send("Done.", hidden=True)
+
+    @cog_ext.cog_subcommand(base="post", name="create", description="Developer - Makes a Blog post.", base_default_permission=False, base_permissions={
+        832594030264975420: [
+            interactions.utils.manage_commands.create_permission(450678229192278036, 2, True)
+        ],
+        838718002412912661: [
+            interactions.utils.manage_commands.create_permission(450678229192278036, 2, True)
+        ]
+    })
+    async def postcreate(self, ctx: interactions.SlashContext, pid: str, title: str, jsx: str):
+        async with aiohttp.ClientSession(headers={"auth": self.token["eggs"]}) as session:
+            await session.post("https://seggs.tk/api/posts", json={
+                "id": pid,
+                "title": title,
+                "jsx": jsx
+            })
+        
+        await ctx.send("Done.", hidden=True)
+
+    @cog_ext.cog_subcommand(base="post", name="delete", description="Developer - Removes a Blog post.", base_default_permission=False, base_permissions={
+        832594030264975420: [
+            interactions.utils.manage_commands.create_permission(450678229192278036, 2, True)
+        ],
+        838718002412912661: [
+            interactions.utils.manage_commands.create_permission(450678229192278036, 2, True)
+        ]
+    })
+    async def postdelete(self, ctx: interactions.SlashContext, pid: str):
+        async with aiohttp.ClientSession(headers={"auth": self.token["eggs"]}) as session:
+            await session.delete(f"https://seggs.tk/api/posts/{pid}")
+ 
+        await ctx.send("Done.", hidden=True)
+
+def setup(bot: commands.Bot):
+    bot.add_cog(Developer(bot))
--- EggsBot/cogs/eggs.py
+++ EggsBot/cogs/eggs.py
@@ -2,6 +2,7 @@
 import discord
 import discord_slash as interactions
 import json
+import aiohttp
 from discord_slash import cog_ext
 from discord.ext import commands
 
@@ -16,9 +17,57 @@ class Eggs(commands.Cog):
         with open("./ServerEggs/EggsBot/assets/embed.json") as embedfile:
             self.embed = json.load(embedfile)
 
+    async def template(self, ectx: interactions.SlashContext, name: str, author: discord.User, guild: discord.Guild, picture: str):
+        for key, value in self.slash.commands.items():
+            if key == name:
+                desc: str = value.description
+                description = desc.lstrip("Eggs - ")
+                break
+
+        e = discord.Embed(title=f"The \"{name}\" Egg", color=int(self.embed["color"], 16), description=f"**Uploaded by `{str(author)} ({author.id})` from `{guild.name}`.**\n\"{description}\"")
+        e.set_author(name=self.embed["author"] + "Eggs", icon_url=self.embed["icon"])
+        e.set_image(url=picture)
+        e.set_footer(text=self.embed["footer"], icon_url=self.embed["icon"])
+        await ectx.send(embed=e)
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        async with aiohttp.ClientSession() as session:
+            async with session.get("https://seggs.tk/api/eggs") as response:
+                eggs = await response.json()
+
+        for egg in eggs["eggs"]:
+            async def eggcoro(ectx: interactions.SlashContext):
+                await self.template(
+                    ectx,
+                    egg["name"],
+                    await self.bot.fetch_user(int(egg["data"]["uploader"])),
+                    self.bot.get_guild(int(egg["data"]["guild"])),
+                    egg["pic"]
+                )
+
+            description = egg["desc"]
+            self.slash.add_slash_command(eggcoro, egg["name"], f"Eggs - {description}", [int(egg["data"]["guild"])])
+
+        await self.slash.sync_all_commands()
+
     @cog_ext.cog_subcommand(base="egg", name="random", description="Eggs - Gets a random Egg from the SEggs API.")
     async def eggrandom(self, ctx: interactions.SlashContext):
-        await ctx.send("**Coming soon!**")
+        async with aiohttp.ClientSession() as session:
+            async with session.get("https://seggs.tk/api/eggs/random") as response:
+                egg = await response.json()
+                name = egg["name"]
+                user = await self.bot.fetch_user(int(egg["data"]["uploader"]))
+                guild = self.bot.get_guild(int(egg["data"]["guild"]))
+                botver = egg["data"]["botver"]
+                desc = egg["desc"]
+                pic = egg["pic"]
+        
+        e = discord.Embed(title=f"The \"{name}\" Egg", color=int(self.embed["color"], 16), description=f"**Uploaded by `{str(user)} ({user.id})` from `{guild.name}`.**\n\"{desc}\"")
+        e.set_author(name=self.embed["author"] + "Eggs", icon_url=self.embed["icon"])
+        e.set_image(url=pic)
+        e.set_footer(text=self.embed["footer"], icon_url=self.embed["icon"])
+        await ctx.send(embed=e)
 
     @cog_ext.cog_subcommand(base="egg", name="create", description="Eggs - Create an Egg on this server!", options=[
         interactions.utils.manage_commands.create_option("name", "The name of the Egg.", 3, True),
@@ -27,19 +76,24 @@ class Eggs(commands.Cog):
     ])
     @commands.has_permissions(manage_guild=True)
     async def eggcreate(self, ctx: interactions.SlashContext, name: str, description: str, picture: str):
-        async def eggcoro(ectx: interactions.SlashContext):
-            for key, value in self.slash.commands.items():
-                if key == name:
-                    desc: str = value.description
-                    description = desc.lstrip("Eggs - ")
-                    break
+        async with aiohttp.ClientSession(headers={"auth": self.token["eggs"]}) as session:
+            async with session.post("https://seggs.tk/api/eggs", json={
+                "name": name,
+                "data": {
+                    "uploader": ctx.author.id,
+                    "guild": ctx.guild.id,
+                    "botver": self.bot.version
+                },
+                "desc": description,
+                "pic": picture
+            }) as response:
+                if response.status == 400:
+                    await ctx.send("It appears an Egg with that name already exists.", hidden=True)
+                    return
 
-            e = discord.Embed(title=f"The \"{name}\" Egg", color=int(self.embed["color"], 16), description=f"**Uploaded by `{str(ctx.author)} ({ctx.author.id})`.**\n\"{description}\"")
-            e.set_author(name=self.embed["author"] + "Eggs", icon_url=self.embed["icon"])
-            e.set_image(url=picture)
-            e.set_footer(text=self.embed["footer"], icon_url=self.embed["icon"])
-            await ectx.send(embed=e)
-        
+        async def eggcoro(ectx: interactions.SlashContext):
+            await self.template(ectx, name, ctx.author, ctx.guild, picture)
+
         self.slash.add_slash_command(eggcoro, name.lower(), f"Eggs - {description}", [ctx.guild.id])
         await self.slash.sync_all_commands()
 
@@ -56,6 +110,9 @@ class Eggs(commands.Cog):
                 await interactions.utils.manage_commands.remove_slash_command(self.bot.user.id, self.token["eggs"], ctx.guild.id, cmdid)
                 await self.slash.sync_all_commands()
                 break
+
+        async with aiohttp.ClientSession(headers={"auth": self.token["eggs"]}) as session:
+            await session.delete(f"https://seggs.tk/api/eggs/{name}")
 
         await ctx.send("Egg deleted successfully.", hidden=True)
 
